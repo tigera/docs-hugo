dbded8c11980d4b4e6bab508224c1790f92d399d
Doug Collier <doug@tigera.io>
2022-02-23 09:52:45 -0500

    cleanup

--- merge/merge.sh
+++ merge/merge.sh
@@ -1,51 +1,49 @@
 #!/usr/bin/env bash
 set -euo pipefail
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(realpath "${SCRIPT_DIR}")"
 
-# change dir to script location
-cd "$(dirname "$0")"
+function cdsl() {
+  # change dir to script location
+	cd "$SCRIPT_DIR"
+}
+
+function docs_merge() {
+  cdsl
+  cd docs
+	local name=$1
+  git remote add $name ../$name
+  git fetch $name
+  git merge --allow-unrelated-histories --no-edit $name/master
+  git remote remove $name
+	cdsl
+}
+
+# set pwd to this script location
+cdsl
 
 # create unified docs repository
 rm -rf ./docs
-mkdir -p ./docs
-cd docs
-git init
-cd -
+git init docs
 
 # process open source
 rm -rf ./calico/
 git clone git@github.com:projectcalico/calico.git
 cd calico 
 git filter-repo --path calico/
-cd -
-cd docs
-git remote add calico ../calico
-git fetch calico
-git merge --allow-unrelated-histories --no-edit calico/master
-git remote remove calico
-cd -
+docs_merge calico
 
 # process enterprise
 rm -rf ./calico-private/
 git clone git@github.com:tigera/calico-private.git
 cd calico-private
 git filter-repo --path calico/ --path-rename calico/:calico-enterprise/
-cd -
-cd docs
-git remote add calico-private ../calico-private
-git fetch calico-private
-git merge --allow-unrelated-histories --no-edit calico-private/master
-git remote remove calico-private
-cd -
+docs_merge calico-private
 
 # process cloud
 rm -rf ./calico-cloud/
 git clone git@github.com:tigera/calico-cloud.git
 cd calico-cloud
 git filter-repo --to-subdirectory-filter calico-cloud/
-cd -
-cd docs
-git remote add calico-cloud ../calico-cloud
-git fetch calico-cloud
-git merge --allow-unrelated-histories --no-edit calico-cloud/master
-git remote remove calico-cloud
-cd -
+docs_merge calico-cloud
+
