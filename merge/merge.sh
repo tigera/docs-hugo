49f4c3704927b0c112f602edac4f6258e90b5919
Doug Collier <doug@tigera.io>
2022-02-23 14:24:05 -0500

    set calico as origin of jekyll, add md files

--- merge/merge.sh
+++ merge/merge.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
-SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
-SCRIPT_DIR="$(realpath "${SCRIPT_DIR}")"
+SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 
-function cdsl() {
+function home() {
   # change dir to script location
-	cd "$SCRIPT_DIR"
+  cd "$SCRIPT_DIR"
 }
 
-function docs_merge() {
-  cdsl
+function merge() {
+  home
   cd docs
-	local name=$1
+  local name=$1
   git remote add $name ../$name
   git fetch $name
   git merge --allow-unrelated-histories --no-edit $name/master
   git remote remove $name
-	cdsl
+  home
 }
 
 # set pwd to this script location
-cdsl
+home
 
 # create unified docs repository
 rm -rf ./docs
@@ -30,20 +29,21 @@ git init docs
 rm -rf ./calico/
 git clone git@github.com:projectcalico/calico.git
 cd calico 
-git filter-repo --path calico/
-docs_merge calico
+git filter-repo --path calico/ --path-rename calico/:
+merge calico
 
 # process enterprise
 rm -rf ./calico-private/
 git clone git@github.com:tigera/calico-private.git
 cd calico-private
 git filter-repo --path calico/ --path-rename calico/:calico-enterprise/
-docs_merge calico-private
+git filter-repo --path-glob "**/*.md"
+merge calico-private
 
 # process cloud
 rm -rf ./calico-cloud/
 git clone git@github.com:tigera/calico-cloud.git
 cd calico-cloud
 git filter-repo --to-subdirectory-filter calico-cloud/
-docs_merge calico-cloud
-
+git filter-repo --path-glob "**/*.md"
+merge calico-cloud
