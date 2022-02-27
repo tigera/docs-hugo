4890d6cee711825b2cb03087ebf9b1bd28002619
Doug Collier <doug@tigera.io>
2022-02-27 09:11:13 -0500

    fix more issues

--- merge/merge.sh
+++ merge/merge.sh
@@ -27,15 +27,22 @@ git init docs
 rm -rf ./calico/
 git clone git@github.com:projectcalico/calico.git
 cd calico 
-git filter-repo --path calico/ --path-rename calico/: --path-rename _data/versions.yml:calico/_data/versions.yml
+git filter-repo  --path calico/ --path-rename calico/:
+mkdir -p calico/_data
+cp _data/versions.yml calico/_data/versions.yml
+git add .
+git commit -m "copy versions.yml"
 merge calico
 
 # process enterprise
+rm -rf ./libcalico-go
+mkdir -p ./libcalico-go
 rm -rf ./calico-private/
 git clone git@github.com:tigera/calico-private.git
 cd calico-private
+cp -r libcalico-go/* ../libcalico-go
 git filter-repo --path calico/ --path-rename calico/:calico-enterprise/
-git filter-repo --path-glob "**/*.md" --path _includes/ --path _data/
+git filter-repo --path-glob "**/*.md" --path calico-enterprise/_includes/ --path calico-enterprise/_data/
 merge calico-private
 
 # process cloud
@@ -43,5 +50,5 @@ rm -rf ./calico-cloud/
 git clone git@github.com:tigera/calico-cloud.git
 cd calico-cloud
 git filter-repo --to-subdirectory-filter calico-cloud/
-git filter-repo --path-glob "**/*.md" --path _includes/ --path _data/
+git filter-repo --path-glob "**/*.md" --path calico-cloud/_includes/ --path calico-cloud/_data/
 merge calico-cloud
