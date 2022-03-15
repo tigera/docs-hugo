8b8f438a108ab068fc154497cb1451faaab0bb9e
Doug Collier <doug@tigera.io>
2022-03-15 14:23:58 -0400

    fix regex

--- merge/merge.sh
+++ merge/merge.sh
@@ -103,9 +103,9 @@ function hugo_fixup() {
   else:
     return filename
   '
-  find . -type f -print0 | xargs -0 sed -r -i 's/\{%\s*include\s+\/([a-zA-Z0-9\-\/\.]+?)%}/{{ partial \1 }}/g'
-  find . -type f -print0 | xargs -0 sed -r -i "s/\{%\s*include\s+([a-zA-Z0-9\-\/\.]+?)%}/{{ partial ${name}\/\1 }}/g"
-  find . -type f -print0 | xargs -0 sed -r -i "s/\{%\s*include_cached\s+([a-zA-Z0-9\-\/\.]+?)%}/{{ partial ${name}\/\1 }}/g"
+  find . -type f -print0 | xargs -0 sed -r -i 's/\{%\s*include\s+\/([a-zA-Z0-9\-\/\.]+)%}/{{ partial \1 }}/g'
+  find . -type f -print0 | xargs -0 sed -r -i "s/\{%\s*include\s+([a-zA-Z0-9\-\/\.]+)%}/{{ partial ${name}\/\1 }}/g"
+  find . -type f -print0 | xargs -0 sed -r -i "s/\{%\s*include_cached\s+([a-zA-Z0-9\-\/\.]+)%}/{{ partial ${name}\/\1 }}/g"
   git add .
   git commit -m "updating content for hugo"
 }
